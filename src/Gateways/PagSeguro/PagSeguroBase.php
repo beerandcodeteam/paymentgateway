9aa4767450d0950e1a5e9639d0d778eb240b34f0
Diogo Lindoso <diogolindoso@gmail.com>
2020-08-06 14:11:54 -0400

    Definido token headers e funcoes

--- src/Gateways/PagSeguro/PagSeguroBase.php
+++ src/Gateways/PagSeguro/PagSeguroBase.php
@@ -2,7 +2,20 @@
 
 namespace BeerAndCodeTeam\PaymentGateway\Gateways\PagSeguro;
 
-class PagSeguroBase
+abstract class PagSeguroBase
 {
+    /** @var String $token receives PAGSEGURO_TOKEN from .env */
+    protected $token = env('PAGSEGURO_TOKEN', false);
 
+    /** @var array $headers default header for PagSeguro requests */
+    protected $headers = [
+        'Content-Type' => 'application/json',
+        'Authorization' => $this->token,
+        'x-api-version' => '4.0',
+    ];
+    abstract function charge(array $values);
+
+    abstract function findCharge(String $chargeId);
+
+    abstract function refound(String $chargeId);
 }
