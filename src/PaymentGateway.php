94365114fa2bdafdd4fc190f688ea86c823fb8bc
fsrocha.dev <contato.frankrocha@gmail.com>
2020-08-06 00:10:04 -0300

    Documentando a classe PaymentGateway

--- src/PaymentGateway.php
+++ src/PaymentGateway.php
@@ -4,15 +4,33 @@ namespace BeerAndCode\PaymentGateway;
 
 class PaymentGateway
 {
+    /**
+     * Methods accepted by the package
+     *
+     * @var array
+     */
     private $acceptedMethods = [
-        'Pagseguro', 'PayPal'
+        'PagSeguro', 'PayPal'
     ];
 
+    /**
+     * Method validated in the constructor
+     *
+     * @var string
+     */
+    private $method;
+
+    /**
+     * @param string $method
+     */
     public function __construct(string $method)
     {
         if (!Arr::exists($this->acceptedMethods, $method)) {
             throw new Exception("Method not accepted.", 1);
         }
+
+        $this->method = $method;
+
         $this->initialize();
     }
 }
