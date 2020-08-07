f999ceb713659a0dcfce9af74954d37f4ec6a138
Rogerio Lamarques <rogerio@lamarques.com.br>
2020-08-06 23:34:09 -0300

    adicionado providers para listar classes por gateway

--- config/config.php
+++ config/config.php
@@ -9,7 +9,19 @@ return [
         'PAYMENT_GATEWAY_APP_ID' => env('PAYMENT_GATEWAY_ENV', 'app4088593308'),
         'PAYMENT_GATEWAY_APP_KEY' => env('PAYMENT_GATEWAY_ENV', '48BE7F25FEFE67DBB4292F9524496612'),
         'envinronment' => env('PAYMENT_GATEWAY_ENV', 'sandbox')
-    ]
+    ],
+
+
+
+
+
+    /*
+     * Providers
+     */
+    'providers' => [
+        'pagseguro' => \BeerAndCodeTeam\PaymentGateway\Gateways\PagSeguro\PagSeguro::class,
+        'paypal' => \BeerAndCodeTeam\PaymentGateway\Gateways\PayPal\PayPal::class
+    ],
 ];
 
 
