ef8b651a49d9246e54604eaeea12186efebee88e
fsrocha.dev <contato.frankrocha@gmail.com>
2020-08-06 23:51:23 -0300

    Ajuste do path do config e remoção de comentários

--- src/PaymentGatewayServiceProvider.php
+++ src/PaymentGatewayServiceProvider.php
@@ -11,36 +11,10 @@ class PaymentGatewayServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'payment-gateway');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'payment-gateway');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('paymentgateway.php'),
+                __DIR__.'/../config/config.php' => config_path('./config/config.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/payment-gateway'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/payment-gateway'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/payment-gateway'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
         }
     }
 
