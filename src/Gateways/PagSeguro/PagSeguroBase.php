e6aa19bd92d138849caa03a9c97df394c8a749f8
Diogo Lindoso <diogolindoso@gmail.com>
2020-08-10 15:32:50 -0400

    Definido  recebe chave PAGSEGURO_TOKEN

--- src/Gateways/PagSeguro/PagSeguroBase.php
+++ src/Gateways/PagSeguro/PagSeguroBase.php
@@ -4,5 +4,6 @@ namespace BeerAndCodeTeam\PaymentGateway\Gateways\PagSeguro;
 
 class PagSeguroBase
 {
-
+    /** @var String $token receives PAGSEGURO_TOKEN from .env */
+    protected $token = env('PAGSEGURO_TOKEN', false);
 }
