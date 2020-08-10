09d6a7a8542c1e1df7765fd2b624d2c378c29549
Diogo Lindoso <diogolindoso@gmail.com>
2020-08-10 15:36:03 -0400

    Definido padrao de header para requisicoes api PagSeguro

--- src/Gateways/PagSeguro/PagSeguroBase.php
+++ src/Gateways/PagSeguro/PagSeguroBase.php
@@ -6,4 +6,11 @@ class PagSeguroBase
 {
     /** @var String $token receives PAGSEGURO_TOKEN from .env */
     protected $token = env('PAGSEGURO_TOKEN', false);
+
+    /** @var array $headers default header for PagSeguro requests */
+    protected $headers = [
+        'Content-Type' => 'application/json',
+        'Authorization' => $this->token,
+        'x-api-version' => '4.0',
+    ];
 }
