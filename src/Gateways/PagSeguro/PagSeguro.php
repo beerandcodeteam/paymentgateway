8ac07892109478feb2e29a837516b9c692b794d0
Diogo Lindoso <diogolindoso@gmail.com>
2020-08-12 12:14:48 -0400

    feat implementado funçao para buscar transacoes no pagseguro

--- src/Gateways/PagSeguro/PagSeguro.php
+++ src/Gateways/PagSeguro/PagSeguro.php
@@ -2,10 +2,23 @@
 
 namespace BeerAndCodeTeam\PaymentGateway\Gateways\PagSeguro;
 
+use Illuminate\Support\Facades\Http;
+
 class PagSeguro extends PagSeguroBase
 {
     public function pay()
     {
         return 'Pagando com pagseguro';
+    }
+
+    public function searchByTransactionCode(String $transactionCode)
+    {
+        try {
+            $response = Http::withHeaders($this->headers)
+                ->get($this->baseUrl . 'charges/' . $transactionCode);
+            return collect($response->json());
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
     }
 }
