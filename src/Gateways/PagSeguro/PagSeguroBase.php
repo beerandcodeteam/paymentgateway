a8c1de4c36992f1ca0e17727a7d1a7073f411c20
Diogo Lindoso <diogolindoso@gmail.com>
2020-08-12 02:29:22 -0400

    Refatorado classe PagSeguroBase.php

--- src/Gateways/PagSeguro/PagSeguroBase.php
+++ src/Gateways/PagSeguro/PagSeguroBase.php
@@ -4,16 +4,27 @@ namespace BeerAndCodeTeam\PaymentGateway\Gateways\PagSeguro;
 
 class PagSeguroBase
 {
-    /** @var String $token receives PAGSEGURO_TOKEN from .env */
-    protected $token = env('PAGSEGURO_TOKEN', false);
+    /** @var String $token receives PG_PAGSEGURO_KEY from .env */
+    protected $token;
 
     /** @var array $headers default header for PagSeguro requests */
-    protected $headers = [
-        'Content-Type' => 'application/json',
-        'Authorization' => $this->token,
-        'x-api-version' => '4.0',
-    ];
+    protected $headers;
 
-    /** @var array $baseUrl base url for pagseguro's api */
+    /** @var array $baseUrl base url for pagseguro api */
     protected $baseUrl = 'https://sandbox.api.pagseguro.com/';
+
+    public function __construct()
+    {
+        $this->token = config('paymentgateway.providers.pagseguro.key');
+
+        $this->headers = [
+            'Content-Type' => 'application/json',
+            'Authorization' => $this->token,
+            'x-api-version' => '4.0',
+        ];
+
+        if (strtolower(config('paymentgateway.providers.pagseguro.environment')) == 'production') {
+            $this->baseUrl = 'https://api.pagseguro.com/';
+        }
+    }
 }
