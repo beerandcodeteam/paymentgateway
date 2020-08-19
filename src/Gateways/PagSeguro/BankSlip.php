6ccae01adeae3ceee1115bb50efffa074baaf211
Diogo Lindoso <diogolindoso@gmail.com>
2020-08-18 20:43:02 -0400

    Feature implementado metodo toArray()

--- src/Gateways/PagSeguro/BankSlip.php
+++ src/Gateways/PagSeguro/BankSlip.php
@@ -12,6 +12,30 @@ class BankSlip implements PaymentMethodInterface
     private Adress $adress;
     public function toArray(): array
     {
-        return [];
+        return [
+            "type" => "BOLETO",
+            "boleto" => [
+                "due_date" => $this->dueDate,
+                "instruction_lines" => [
+                    "line_1" => $this->instructionsLines[0],
+                    "line_2" => $this->instructionsLines[0]
+                ],
+                "holder" => [
+                    "name" => $this->holder->getName(),
+                    "tax_id" => $this->holder->getTaxId(),
+                    "email" => $this->holder->getEmail(),
+                    "address" => [
+                        "country" => $this->adress->getCountry(),
+                        "region" => $this->adress->getRegion(),
+                        "region_code" => $this->adress->getRegionCode(),
+                        "city" => $this->adress->getCity(),
+                        "postal_code" => $this->adress->getPostalCode(),
+                        "street" => $this->adress->getStreet(),
+                        "number" => $this->adress->getNumber(),
+                        "locality" => $this->adress->getLocality()
+                    ]
+                ]
+            ]
+        ];
     }
 }
