5d4490d3befb60b1faaa7cb563c87e841b209c11
Diogo Lindoso <diogolindoso@gmail.com>
2020-08-19 16:03:49 -0400

    Feature iniciado classe Address.php

---
+++ src/Gateways/PagSeguro/Address.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BeerAndCodeTeam\PaymentGateway\Gateways\PagSeguro;
+
+class Address
+{
+   private string $country;
+   private string $region;
+   private string $regionCode;
+   private string $city;
+   private string $postalCode;
+   private string $street;
+   private string $number;
+   private string $locality;
+   
+
+}
