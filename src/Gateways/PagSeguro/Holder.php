dffadb9e08e2355910223427346cee243df0ed78
Diogo Lindoso <diogolindoso@gmail.com>
2020-08-19 01:07:21 -0400

    Feature definido metodo setName

--- src/Gateways/PagSeguro/Holder.php
+++ src/Gateways/PagSeguro/Holder.php
@@ -8,4 +8,12 @@ class Holder
     private string $taxId;
     private string $email;
     private Address $address;
+
+    public function setName(string $name)
+    {
+        if (strlen($name) < 1  || strlen($name) > 30) {
+            throw new \Exception("Error:  the name most be 1-30 characters. It has " . strlen($name));
+        }
+        $this->name = $name;
+    }
 }
