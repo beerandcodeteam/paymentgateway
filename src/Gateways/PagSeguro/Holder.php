917669374159c1494fc765452cad039c90ca551d
Diogo Lindoso <diogolindoso@gmail.com>
2020-08-19 00:59:56 -0400

    Feature definicao da classe holder

---
+++ src/Gateways/PagSeguro/Holder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BeerAndCodeTeam\PaymentGateway\Gateways\PagSeguro;
+
+class Holder
+{
+    private string $name;
+    private string $taxId;
+    private string $email;
+    private Address $address;
+}
